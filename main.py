9140197332b9f21c02ad67edbb89c5422016acc9
Andreea Megherlich <andreea.meghe@gmail.com>
2023-12-23 10:26:37 +0200

    Compiled log lines into data structures

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
+import re
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+log_file = open("output.txt", "r")
+log_lines = log_file.readlines()
 
+compiled_logs = []
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+ERROR_log_pattern = re.compile(r'(\d{2}:\d{2}:\d{2}) - \[([A-Z]+)\] - (\w+) has (.+?) after (\d+)ms.?.*')
+DEBUG_log_pattern = re.compile(r'(\d{2}:\d{2}:\d{2}) - \[([A-Z]+)\] - (\w+) is still running, please wait...*')
+INFO1_log_pattern = re.compile(r'(\d{2}:\d{2}:\d{2}) - \[([A-Z]+)\] - (\w+) has started running...*')
+INFO2_log_pattern = re.compile(r'(\d{2}:\d{2}:\d{2}) - \[([A-Z]+)\] - (\w+) has (.+?) in (\d+)ms.?.*')
 
+for log_line in log_lines:
+    ERROR_match = ERROR_log_pattern.match(log_line)
+    DEBUG_match = DEBUG_log_pattern.match(log_line)
+    INFO1_match = INFO1_log_pattern.match(log_line)
+    INFO2_match = INFO2_log_pattern.match(log_line)
+    if ERROR_match:
+        timestamp, log_type, app, status, run_time = ERROR_match.groups()
+        compiled_logs.append({"timestamp": timestamp, "log_type": log_type, "app": app, "status": -1, "run_time": run_time})
+    elif DEBUG_match:
+        timestamp, log_type, app = DEBUG_match.groups()
+        compiled_logs.append({"timestamp": timestamp, "log_type": log_type, "status": 0, "app": app})
+    elif INFO1_match:
+        timestamp, log_type, app = INFO1_match.groups()
+        compiled_logs.append({"timestamp": timestamp, "log_type": log_type, "status": 0, "app": app})
+    elif INFO2_match:
+        timestamp, log_type, app, status, run_time = INFO2_match.groups()
+        compiled_logs.append({"timestamp": timestamp, "log_type": log_type, "app": app, "status": 1, "run_time": run_time})
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+for log in compiled_logs:
+    print(log)
